4477886de0868484c2dcd7802711a1aa754151ef
Dave Berner <davidajberner@gmail.com>
2022-10-27 11:59:30 +1100

    update simple logs

--- src/index.js
+++ src/index.js
@@ -2,20 +2,33 @@ const {authToken, getPem} = require('@kinde-oss/kinde-node-auth-utils').default;
 
 const kindeExpress = (domain) => {
   return async (req, res, next) => {
-    const pem = await getPem(domain);
+    try {
+      const pem = await getPem(domain);
 
-    const authHeader = req.headers.authorization;
-    const token = authHeader && authHeader.split(' ')[1];
+      const authHeader = req.headers.authorization;
+      const token = authHeader && authHeader.split(' ')[1];
 
-    if (token == null) return res.sendStatus(401);
+      if (token == null) {
+        console.log('no token found');
+        return res.sendStatus(401);
+      }
 
-    authToken(token, pem, (err, user) => {
-      if (err) return res.sendStatus(403);
-      const userObj = JSON.parse(user);
-      req.user = {id: userObj.sub};
+      authToken(token, pem, (err, user) => {
+        if (err) {
+          console.log(err);
+          return res.sendStatus(403);
+        }
+        const userObj = JSON.parse(user);
+        if (userObj) {
+          console.log('User found');
+        }
+        req.user = {id: userObj.sub};
 
-      next();
-    });
+        next();
+      });
+    } catch (err) {
+      console.log(err);
+    }
   };
 };
 
